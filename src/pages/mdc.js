c2587270d6873d18d142ab9de66d9298e99cf921
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-03-21 12:42:03 -0300

    feat: add logic of mdc page

--- src/pages/mdc.js
+++ src/pages/mdc.js
@@ -1,9 +1,51 @@
-import {} from 'react'
+import {useState} from 'react'
 import Layout from '../components/Layout'
+import { mdc } from '../functions/algorithmFunc'
 import {Main,Title,RowInputContainer,Input} from '../styles/StylePageV1'
-import {Button} from '../styles/StylePageV2'
+import {Button, Result} from '../styles/StylePageV2'
 
 export default function Mdc() {
+  const [numbers,setNumbers] = useState({
+    valueA:null,
+    valueB:null
+  })
+
+  const [mdcResult,setMdcResult] = useState(null)
+
+  function handleValueA(value){
+    if(isNaN(value)){
+      setNumbers({valueA:null,valueB:numbers.valueB})
+      setMdcResult(null)
+      return 
+    }
+
+    setNumbers({valueA:value,valueB:numbers.valueB})
+  }
+
+  function handleValueB(value){
+    console.log('value: '+value)
+    if(isNaN(value)){
+      setNumbers({valueA:numbers.valueA,valueB:null})
+      setMdcResult(null)
+      return 
+    }
+
+    setNumbers({valueA:numbers.valueA,valueB:value})
+  }
+
+  function isValid(){
+    return numbers.valueA !== null && numbers.valueB !== null;
+  }
+
+  function updateMdc(){
+    if(!isValid())
+      return 
+    
+    const value = mdc(numbers.valueA,numbers.valueB)
+    setMdcResult(value);
+  }
+
+
   return (
     <Layout 
         title="Máximo Divisor Comum" 
@@ -12,10 +54,11 @@ export default function Mdc() {
         <Main>
             <Title>Digite os numeros que deseja calcular o MDC</Title>
             <RowInputContainer>
-                <Input type="number"/>
-                <Input type="number"/>
+                <Input type="number" onChange={({target})=>handleValueA(target.valueAsNumber)}/>
+                <Input type="number" onChange={({target})=>handleValueB(target.valueAsNumber)}/>
             </RowInputContainer>
-            <Button>Calcular</Button>
+            <Button onClick={()=>updateMdc()}>Calcular</Button>
+            {mdcResult&&<Result>O MDC é igual a <b>{mdcResult}</b></Result>}
         </Main>
     </Layout>
   )
