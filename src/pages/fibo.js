0b4dbf762b94d17d561968aeb64f6b5e984df5fe
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-03-21 11:36:55 -0300

    feat: create logic of fibo page

--- src/pages/fibo.js
+++ src/pages/fibo.js
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
-import {Main,Title,InputContainer,Input} from '../styles/StylePageV1'
+import { fibonacci,fibonacciWithouRecursion } from "../functions/algorithmFunc";
+import { Main, Title, InputContainer, Input } from '../styles/StylePageV1'
 
 export default function Fibo() {
-  return (
-    <Layout title="Fibonacci" description="Algoritmo que vai receber mostrar a fibonnacci de um número inteiro">
-        <Main>
-            <InputContainer>
-                <Title>Digite o número</Title>
-                <Input type="number"/>
-            </InputContainer>
-        </Main>
-    </Layout>
-  )
+    const [number,setNumber] = useState(null)
+
+    function renderResult(){
+        if(isNaN(number) || number<=0){
+            setNumber(null)
+            return 
+        }
+
+        let result = '';
+        for(let i = 0; i<number; i++){
+            const fibo = fibonacciWithouRecursion(i+1)
+            result += ' '+fibo
+        }   
+
+        return <h1>{result}</h1>
+    }
+
+    return (
+        <Layout title="Fibonacci" description="Algoritmo que vai receber mostrar a fibonnacci de um número inteiro">
+            <Main>
+                <InputContainer>
+                    <Title>Digite um número inteiro maior que zero</Title>
+                    <Input type="number" min={0} onChange={({target})=>setNumber(target.valueAsNumber)}/>
+                </InputContainer>
+                {
+                    number != null && renderResult()
+                }
+            </Main>
+        </Layout>
+    )
 }
